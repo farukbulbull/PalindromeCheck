e22f8dfbc9990d82ba92577b0d3906215cf74f23
farukbulbull <farukblbl34@gmail.com>
2024-10-05 10:56:34 +0300

    Create PalindromeCheck.java

---
+++ PalindromeCheck.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int number = scanner.nextInt();
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        if (originalNumber == reversedNumber) {
+            System.out.println(originalNumber + " bir palindrom sayıdır.");
+        } else {
+            System.out.println(originalNumber + " bir palindrom sayı değildir.");
+        }
+    }
+}
